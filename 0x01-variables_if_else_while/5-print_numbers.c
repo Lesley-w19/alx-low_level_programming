f6e1c80a4dc17e03bd4665d90c5475c7087fb92c
Lesley-w19 <lesley.kamamo@gmail.com>
2022-03-14 14:03:21 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - use putchar to print alphabet
+ *
+ * Description: print alphabets in lowercase.
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		printf("%d", a);
+	}
+	putchar('\n');
+	return (0);
+}
