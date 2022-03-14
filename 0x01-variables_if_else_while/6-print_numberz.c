23df11dd12e185f65f2cb16b5aced12aca47571d
Lesley-w19 <lesley.kamamo@gmail.com>
2022-03-14 14:11:32 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints numbers between 0 to 9.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}
