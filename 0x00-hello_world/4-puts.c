23b51f1322b1355ab2a96cee423b31993db325d3
Lesley-w19 <lesley.kamamo@gmail.com>
2022-03-14 14:26:33 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print out the given line
+ *
+ * Descriptive: use the function puts
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
