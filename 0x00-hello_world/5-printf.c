65872cc46fcfb7305728a851ac95a2164390cb22
Lesley-w19 <lesley.kamamo@gmail.com>
2022-03-14 14:32:20 -0700

    Hello, prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print the given line
+ *
+ * Descriptive: use printf
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+		return (0);
+}
