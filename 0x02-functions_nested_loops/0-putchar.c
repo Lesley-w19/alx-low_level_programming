420e8a6c16daec2969d344c591d1efd9a402c4a2
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-03-16 08:16:21 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Description: should have a new line after print
+ *
+ * Return:0 (success)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
+
