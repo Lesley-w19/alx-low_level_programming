b589736a42adf43a01ccb3ef45cd7a90d56e81c9
Lesley-w19 <lesley.kamamo@gmail.com>
2022-03-29 13:51:34 -0700

    2. strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/*
+ *  _strchr - locates a character in a string,
+ *  @s: string.
+ *  @c: character.
+ *  Return: the pointer to the first occurrence of the character c.
+ */
+char *_strchr(char *s, char c)
+{
+	unsigned int a;
+
+	for ( a = 0; *(s + a) != '\0'; a++)
+		if (*(s + a) == c)
+			return (s + a);
+	if (*(s + a) == c)
+		return (s + a);
+	return ('\0');
+}
+
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -1,3 +1,4 @@
 Even more pointers, arrays and strings
 0. memset
 1. memcpy
+2. strchr
