463e7252872c1bba4e57e976fd02eeac16016411
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-05 01:23:21 -0700

    1. The woman who has no imagination has no wings

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,13 +11,12 @@
 char *_strdup(char *str)
 {
 	char *strout;
-	unsigned a, b;
+	unsigned int a, b;
 
 	if (str == NULL)
 		return (NULL);
 
 	for ( a = 0; str[a] != '\0'; a++)
-		;
 
 	strout = (char *)malloc(sizeof(char) * (a + 1));
 
