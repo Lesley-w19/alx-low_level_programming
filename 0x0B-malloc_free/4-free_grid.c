baa235bece5fca3c6d0a2aea8af3415c10fb9534
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-05 02:19:16 -0700

    4. It's not bragging if you can back it up

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - this frees a two-dimensional grid
+ * @grid: ultidimensional array of integers
+ * @height: height of the grid
+ *
+ * Return: no return
+ */
+
+void free_grid(int **grid, int height)
+{
+	if (grid != NULL && height != NULL)
+	{
+		for (; height >= 0; height--)
+			free(grid[height]);
+		free(grid);
+	}
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -3,3 +3,4 @@ MALLOC _ FREE
 1. The woman who has no imagination has no wings
 2. He who is not courageous enough to take risks will accomplish nothing in life
 3. If you even dream of beating me you'd better wake up and apologize
+4. It's not bragging if you can back it up
