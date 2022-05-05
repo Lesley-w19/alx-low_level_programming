9996ee2fc4a6955cc6cb62bf74290bc9c3f9874f
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-05 01:44:55 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,46 @@
+#include "main.h"
+#include <stlib.h>
+
+/**
+ * str_concat - function that concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: return pointer of an array of chars
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	unsigned int a, b, c, limit;
+	char *strout;
+
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	for (a = 0; s1[a] != '\0'; a++)
+		;
+
+	for (b = 0; s2[b] != '\0'; b++)
+		;
+
+	strout = malloc(sizeof(char) * (a + b + 1));
+
+	if (strout == NULL)
+	{
+		free(strout);
+		return (NULL);
+	}
+
+	for (c = 0; c <= a; c++)
+		strout[c] = str[c];
+
+	limit = b;
+
+	for (b = 0; b <= limit; c++, b++)
+		strout[c] = str[b];
+
+	return (strout);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,3 +1,4 @@
 MALLOC _ FREE
 0. Float like a butterfly, sting like a bee
 1. The woman who has no imagination has no wings
+2. He who is not courageous enough to take risks will accomplish nothing in life
