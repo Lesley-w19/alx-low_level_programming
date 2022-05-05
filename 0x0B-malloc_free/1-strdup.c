f57009ea1fb18fd7678bb16a85f3d6353ab9d41c
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-05 01:20:25 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - function returns a pointer to a newly allocated memory space
+ * @str: string
+ *
+ * Return: returns pointer of an array of strings
+ */
+
+char *_strdup(char *str)
+{
+	char *strout;
+	unsigned a, b;
+
+	if (str == NULL)
+		return (NULL);
+
+	for ( a = 0; str[a] != '\0'; a++)
+		;
+
+	strout = (char *)malloc(sizeof(char) * (a + 1));
+
+	if (strout  == NULL)
+		return (NULL);
+
+	for (b = 0; b <= 1 ; b++)
+		strout[b] = str[b];
+
+	return (strout);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,2 +1,3 @@
 MALLOC _ FREE
 0. Float like a butterfly, sting like a bee
+1. The woman who has no imagination has no wings
