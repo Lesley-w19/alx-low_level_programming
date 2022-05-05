a3576d4dbe883132d4294244d91df7e31e99c086
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-05 00:35:36 -0700

    0. Float like a butterfly, sting like a bee

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,13 +3,14 @@
 
 /**
  * create_array - creates an array of chars
- * size - size of the array
- * c : stores char
+ * size: size of the array
+ * c: stores char
  *
  * Return : pointer of an array of chars
  */
 
-char *create_array(unsigned int size, char c){
+char *create_array(unsigned int size, char c)
+{
 	unsigned int i;
 	char *ar;
 
