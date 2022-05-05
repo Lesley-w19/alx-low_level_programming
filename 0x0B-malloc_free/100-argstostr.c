5f17337206545d2169cab3db6245b2863fa63002
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-05 02:44:12 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all arguments in a program
+ * @ac: argument count
+ * @av: argument vector
+ *
+ * Return: returns a pointer to an array of char
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *agout;
+	int x, y, z, ma;
+
+	if (int ac == NULL)
+		return (NULL);
+
+	for (x = y = 0; y < ac; y++)
+	{
+		if (av[y] == NULL)
+			return (NULL);
+
+		for (z = 0; av[y][z] != '\0'; z++)
+			x++;
+		x++;
+	}
+
+	agout = malloc((x + 1) * sizeof(char));
+
+	if ( agout == NULL)
+	{
+		free(agout);
+		return (NULL);
+	}
+
+	for (y = z = ma; ma < x; y++, ma++)
+	{
+		if (av[y][z] == '\0')
+		{
+			agout[ma] = '\n';
+			y++;
+			ma++;
+			z = 0;
+		}
+
+		if (ma < x - 1)
+			agout[ma] = av[y][z];
+	}
+	agout[ma] = '\0';
+
+	return (agout);
+}
+
+
+
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -4,3 +4,4 @@ MALLOC _ FREE
 2. He who is not courageous enough to take risks will accomplish nothing in life
 3. If you even dream of beating me you'd better wake up and apologize
 4. It's not bragging if you can back it up
+5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe
