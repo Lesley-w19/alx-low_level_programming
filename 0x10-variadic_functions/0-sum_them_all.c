c0b6135a2dd4f460f742908c97c15e8a7a9dc58e
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-20 01:29:53 -0700

    0. Beauty is variable, ugliness is constant

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,21 +11,19 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	va_list numbers;
+	va_list lst;
 	unsigned int x;
-	int sum;
-
-	sum = 0;
+	int sum = 0;
 
 	if (n == 0)
 		return (0);
 
-	va_start(numbers, n);
+	va_start(lst, n);
 
 	for (x = 0; x < n; x++)
-		sum += va_arg(numbers, int);
+		sum += va_arg(lst, int);
 
-	va_end(numbers);
+	va_end(lst);
 
 	return (sum);
 }
