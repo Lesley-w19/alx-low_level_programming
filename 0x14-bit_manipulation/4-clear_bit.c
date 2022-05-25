6415cbd27bcdecb6dfedb95e791d29f4f6f02b2c
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-25 01:03:53 -0700

    4. 100

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * clear_bit - function that sets the vlue of a bit to 0 at any given index.
+ * @: pointer to the unsigned long int.
+ * @index: is the index, starting from 0 of the bi you want to get.
+ *
+ * Return: 1 if it worked, or -1 if an error ocured.
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 63)
+		return (-1);
+	*n &= ~(1 << index);
+
+	return (1);
+}
