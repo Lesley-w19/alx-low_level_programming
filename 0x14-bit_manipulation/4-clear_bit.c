d5e47b09b34b10c52f03f140d95b092aa7a2710e
Lesley-w19 <lesley.kamamo@gmail.com>
2022-05-25 01:10:23 -0700

    4. 100

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -10,10 +10,13 @@
 
 int clear_bit(unsigned long int *n, unsigned int index)
 {
+	unsigned int b;
+
 	if (index > 63)
 		return (-1);
 
-	*n &= ~(1 << index);
+	b = 1 << index;
+	*n &= ~(b);
 
 	return (1);
 }
