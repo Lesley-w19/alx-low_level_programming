862aad822ed4bb26d7f6dbd81b16bbafbf4cbd06
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-11 14:33:11 +0000

    6. Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-int _putchar(char);
+int _putchar(char c);
 void print_rev(char *s);
 void rev_string(char *s);
 void puts2(char *str);
