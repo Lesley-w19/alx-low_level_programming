84bc3d6af1ff2c9e4b3059e97052d431ad7598a8
Lesley-w19 <lesley.kamamo@gmail.com>
2022-06-11 11:53:24 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * _puts_recursion ; prints a string, followed by a new line
- * @s : string
+ * _puts_recursion - prints a string, followed by a new line
+ * @s: string
  *
  * Return: no return
  */
