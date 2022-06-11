99c6611dd4c2c116b2d4980ad7798a657d0ee19b
Lesley-w19 <lesley.kamamo@gmail.com>
2022-06-11 11:48:52 -0700

    1. Without libraries what have we? We have no past and no future

--- 0x09-static_libraries/README.md
+++ 0x09-static_libraries/README.md
@@ -1,1 +1,2 @@
 C-Static Library
+1. Without libraries what have we? We have no past and no future
--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-gcc -Wall -Werror -Wextra -pedantic -std=gnu89 -c *.c
-ar -rc liball.a *.o
-ranlib liball.a
-
+gcc -c *.c ; ar -rc liball.a *.o ;
