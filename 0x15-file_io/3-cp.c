933ac49801785a2bb3c86701203b7ccea8f71184
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-06-11 11:43:48 +0000

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -46,17 +46,17 @@ int main(int argc, char *argv[])
 
 	file_from = open(argv[1], O_RDONLY);
 	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
+	ferror_file(file_from, file_to, argv);
 
 	nchars = 1024;
 	while (nchars == 1024)
 	{
 		nchars = read(file_from, buf, 1024);
 		if (nchars == -1)
-			error_file(-1, 0, argv);
+			ferror_file(-1, 0, argv);
 		nwr = write(file_to, buf, nchars);
 		if (nwr == -1)
-			error_file(0, -1, argv);
+			ferror_file(0, -1, argv);
 	}
 
 	err_close = close(file_from);
