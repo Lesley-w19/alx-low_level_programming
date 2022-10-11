50bba5f65bac8945610a7f4b644b8ea6ee8697e0
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-11 22:01:34 +0300

    2. key -> index -- Write a function that gives you the index of a key.
    Prototype: unsigned long int key_index(const unsigned char *key, unsigned long int size);
    where key is the key
    and size is the size of the array of the hash table
    This function should use the hash_djb2 function that you wrote earlier
    Returns the index at which the key/value pair should be stored in the array of the hash table
    You will have to use this hash function for all the next tasks

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+/**
+ * key_index -  a function that gives you the index of a key. 
+ *
+ *@key: the key
+ *@size: is the size of the array of the hash table
+ *
+ * Returns - the index at which the key/value pair should be
+ * stored in the array of the hash table 
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+return (hash_djb2(key) % size);
+}
--- 0x1A-hash_tables/README.md
+++ 0x1A-hash_tables/README.md
@@ -4,3 +4,4 @@
 ### TASKS
 0. >>> ht = {}
 1. djb2
+2. key -> index
