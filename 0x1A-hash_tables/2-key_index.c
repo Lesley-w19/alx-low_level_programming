95efd9d0fce52639d0f977df5995552648d87ffe
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-11 22:05:57 +0300

    return descrption fo r key_index

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,7 +5,7 @@
  *@key: the key
  *@size: is the size of the array of the hash table
  *
- * Returns - the index at which the key/value pair should be
+ * Return - the index at which the key/value pair should be
  * stored in the array of the hash table
  */
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
