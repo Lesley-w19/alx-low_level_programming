f339f286d9ebbc19618af17f0d927b7d9ca26ca9
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-11 22:08:23 +0300

    the return value description

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,9 +5,9 @@
  *@key: the key
  *@size: is the size of the array of the hash table
  *
- * Return - the index at which the key/value pair should be
- * stored in the array of the hash table
+ * Return - the index at which the key/value pair should be stored
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 return (hash_djb2(key) % size);
