f88e42ca4d0e57772dd7baa68cb40cdf0a730f66
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-14 12:35:39 +0300

    return description: replaced - with :

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -2,10 +2,10 @@
 /**
  * key_index - a function that gives you the index of a key.
  *
- *@key: the key
- *@size: is the size of the array of the hash table
+ * @key: the key
+ * @size: is the size of the array of the hash table
  *
- * Return - the index at which the key/value pair should be stored
+ * Return: the index of the key/value pair
  */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
