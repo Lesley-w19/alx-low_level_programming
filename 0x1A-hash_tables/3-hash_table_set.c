147bd996ebf9f5bf17b0a19e5b24debc8575f5fe
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-14 12:49:30 +0300

    3. >>> ht['betty'] = 'cool' Write a function that adds an element to the hash table.
    Prototype: int hash_table_set(hash_table_t *ht, const char *key, const char *value);
    Where ht is the hash table you want to add or update the key/value to
    key is the key. key can not be an empty string
    and value is the value associated with the key. value must be duplicated. value can be an empty string
    Returns: 1 if it succeeded, 0 otherwise
    In case of collision, add the new node at the beginning of the list
    --- replaced false with 0 and true with 1

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -15,37 +15,37 @@ unsigned long int index = 0;
 hash_node_t *node = NULL, *add_node = NULL, *tmp = NULL;
 
 if (!key || !ht || !ht->array)
-return (false);
+return (0);
 
-if (ht == NULL || key == NULL | value == NULL)
-return (0)
+if (ht == NULL || key == NULL || value == NULL)
+return (0);
 
-index = key_index((unsigned char *)key, ht->size);
+index = key_index((unsigned char *) key, ht->size);
 node = ht->array[index];
 
 if(!node)
 {
-node = malloc(sizeof(hash_node_t))
+node = malloc(sizeof(hash_node_t));
 if (!node)
-return(false);
+return(0);
 node->key = strdup(key), node->value = strdup(value);
 node->next = NULL;
 ht->array[index] = node;
-return (true);
+return (1);
 }
 
 for (tmp = node; tmp; tmp = tmp->next)
 if (strcmp(tmp->key, key) == 0)
 {
 free(tmp->value), tmp->value = strdup(value);
-return (true);
+return (1);
 }
 add_node = malloc(sizeof(hash_node_t));
 if (!add_node)
-return (false);
+return (0);
 add_node->key = strdup(key), add_node->value = strdup(value);
 add_node->next = node;
 ht->array[index] = add_node;
 
-return (true);
+return (1);
 }
