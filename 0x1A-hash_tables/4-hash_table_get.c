a7468d2b13a99b76a8a0f1efe312679b95fa671d
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-14 13:25:17 +0300

    COREECTED-- line over 80 characters (85)

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -5,7 +5,8 @@
  * @ht: is the hash table you want to look into
  * @key: key is the key you are looking for
  *
- * Return: the value associated with the element, or NULL if key couldn’t be found
+ * Return: the value associated with the element,
+ * or NULL if key couldn’t be found
  *
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
