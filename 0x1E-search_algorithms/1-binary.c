765546b679319a14ef27ed49ff934e4bc7dea6d5
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-17 20:19:23 +0300

    removed trailing white space 29,33 added a new blank line

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -26,11 +26,11 @@ printf("\n");
 /**
  * binary_search - a function that searches for a value in a
  * sorted array of integers using the Binary search algorithm
- * 
+ *
  * @array: is a pointer to the first element of the array to search in
  * @size: is the number of elements in array
  * @value: is the value to search for
- * 
+ *
  * Return: the index where value is located
  */
 int binary_search(int *array, size_t size, int value)
