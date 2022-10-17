8c6acc5547ea5b25b59f3feff7887e9821998c09
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-17 22:46:53 +0300

    Write a function that searches for a value in a sorted array of integers using the Interpolation search algorithm

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -12,29 +12,31 @@
  */
 int interpolation_search(int *array, size_t size, int value)
 {
-int low = 0, high = size - 1;
-size_t pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+int start = 0, end = size - 1, mid;
 
 	if (array == NULL)
 		return (-1);
 
-	while (array[high] != array[low])
+	while (array[end] != array[start])
 	{
+		mid = start + (
+			(value - array[start]) * (end - start) / (array[end] - array[start])
+		);
 
-		if (pos > size)
+		if (mid > (int) size)
 		{
-			printf("Value checked array[%ld] is out of range\n", pos);
+			printf("Value checked array[%d] is out of range\n", mid);
 			return (-1);
 		}
 
-		printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
+		printf("Value checked array[%d] = [%d]\n", mid, array[mid]);
 
-		if (value == array[pos])
-			return (pos);
-		else if (value < array[pos])
-			high = pos - 1;
+		if (value == array[mid])
+			return (mid);
+		else if (value < array[mid])
+			end = mid - 1;
 		else
-			low = pos + 1;
+			start = mid + 1;
 	}
 
 	return (-1);
