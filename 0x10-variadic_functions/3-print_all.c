fa97e37c98ffc6475e731bc0cb5a5efb9a26729e
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-31 22:30:06 +0300

    removed trailing spaces.. . reduced code lines

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,20 +1,19 @@
 #include "variadic_functions.h"
 /**
  * print_all- a function that prints anything.
- * @format: is a list of typs of arguments passed to the function.
- *
- * Return: no return.
+ * @format: a list of typs of arguments passed to the func
+ * 
  */
 void print_all(const char * const format, ...)
 {
 	va_list varlist;
-	unsigned int l = 0, m, n = 0;
+	unsigned int l = 0, m = 0, n = 0;
 	char *str;
 	const char t_arg[] = "cifs";
 	va_start(varlist, format);
+
 	while (format && format[l])
 	{
-		m = 0;
 		while (t_arg[m])
 		{
 			if (format[l] == t_arg[m] && n)
