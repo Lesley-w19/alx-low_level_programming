c6b47d4ba3968369e383da9d23d90e9a010795e1
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-31 22:15:30 +0300

    removed white trailing space

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -4,8 +4,7 @@
 
 
 /**
- * add_nodeint_end - a function that adds a new node at the end of a 
- * listint_t list.
+ * add_nodeint_end - a function that adds a new node at the  * end of a listint_t list.
  * @head: pointer to the head of the list.
  * @n: the element.
  *
